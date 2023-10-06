a17a6785532be15865da945858dbe1645b76da2a
Jackcodefiles <146663762+Jackcodefiles@users.noreply.github.com>
2023-10-06 22:51:33 +0530

    Update App array.js

--- App array.js
+++ App array.js
@@ -1,71 +1,71 @@
 
-        dict = {
-            "Artist": {
-                "0": "Tag Alwin Paul",
-                "1": "Tag Alwin Paul",
-                "2": "Tag Alwin Paul",
-                "3": "Tag Alwin Paul",
-                "4": "Tag Alwin Paul",
-                "5": "Tag Alwin Paul",
-                "6": "Tag Alwin Paul",
-                "7": "Tag Alwin Paul",
-                "8": "Tag Alwin Paul",
-                "9": "Tag Alwin Paul",
-                "10": "Tag Alwin Paul",
-                "11": "Tag Alwin Paul",
-                "12": "Tag Alwin Paul",
-                "13": "Tag Alwin Paul"
-            },
-            "song_name": {
-                "0": "\nYesuvae Um Mugam Parkanumae ",
-                "1": "\nAnantha Gnana Sorupa Namoo ",
-                "2": "\nKanninmani Pol Ennai ",
-                "3": "\nUmakaga Naan ",
-                "4": "\nEn Adyaalm Um Mugam ",
-                "5": "\nOh Nanba Yesu Undu",
-                "6": "\nIdhayathin Aalam Arinthavare ",
-                "7": "\nSali Kasakkum Thakuthi ",
-                "8": "\nIdhu Varai Seidha ",
-                "9": "\nYesuvae Um Paasathal ",
-                "10": "\nKaala Kaalangal Kathirundhum ",
-                "11": "\nEnnai Ninaitheerae ",
-                "12": "\nElunthar Iraivan Jeyame ",
-                "13": "\nUnga Prasanathil Siragu "
-            },
-            "Song_Link": {
-                "0": "https:\/\/www.christianslavetamil.in\/2023\/01\/yesuvae-um-mugam-parkanumae\/",
-                "1": "https:\/\/www.christianslavetamil.in\/2022\/12\/anantha-gnana-sorupa-namoo-nama\/",
-                "2": "https:\/\/www.christianslavetamil.in\/2022\/06\/kanninmani-pol-ennai-padhukathu\/",
-                "3": "https:\/\/www.christianslavetamil.in\/2022\/01\/umakaga-naan-umakaga-dhan\/",
-                "4": "https:\/\/www.christianslavetamil.in\/2021\/10\/en-adyaalm-um-mugam\/",
-                "5": "https:\/\/www.christianslavetamil.in\/2021\/09\/oh-nanba-yesu-undu\/",
-                "6": "https:\/\/www.christianslavetamil.in\/2021\/09\/idhayathin-aalam-arinthavare\/",
-                "7": "https:\/\/www.christianslavetamil.in\/2021\/09\/sali-kasakkum-thakuthi\/",
-                "8": "https:\/\/www.christianslavetamil.in\/2021\/09\/idhu-varai-seidha\/",
-                "9": "https:\/\/www.christianslavetamil.in\/2021\/09\/yesuvae-um-paasathal\/",
-                "10": "https:\/\/www.christianslavetamil.in\/2021\/09\/kaala-kaalangal-kathirundhum\/",
-                "11": "https:\/\/www.christianslavetamil.in\/2021\/09\/ennai-ninaitheerae-pathiravano\/",
-                "12": "https:\/\/www.christianslavetamil.in\/2021\/07\/elunthar-iraivan-jeyame\/",
-                "13": "https:\/\/www.christianslavetamil.in\/2020\/07\/unga-prasanathil-siragu\/"
-            }
-            , "audio_link": {
+dict = {
+     "Artist": {
+         "0": "Tag Alwin Paul",
+         "1": "Tag Alwin Paul",
+         "2": "Tag Alwin Paul",
+         "3": "Tag Alwin Paul",
+         "4": "Tag Alwin Paul",
+         "5": "Tag Alwin Paul",
+         "6": "Tag Alwin Paul",
+         "7": "Tag Alwin Paul",
+         "8": "Tag Alwin Paul",
+         "9": "Tag Alwin Paul",
+         "10": "Tag Alwin Paul",
+         "11": "Tag Alwin Paul",
+         "12": "Tag Alwin Paul",
+         "13": "Tag Alwin Paul"
+     },
+     "song_name": {
+         "0": "\nYesuvae Um Mugam Parkanumae ",
+         "1": "\nAnantha Gnana Sorupa Namoo ",
+         "2": "\nKanninmani Pol Ennai ",
+         "3": "\nUmakaga Naan ",
+         "4": "\nEn Adyaalm Um Mugam ",
+         "5": "\nOh Nanba Yesu Undu",
+         "6": "\nIdhayathin Aalam Arinthavare ",
+         "7": "\nSali Kasakkum Thakuthi ",
+         "8": "\nIdhu Varai Seidha ",
+         "9": "\nYesuvae Um Paasathal ",
+         "10": "\nKaala Kaalangal Kathirundhum ",
+         "11": "\nEnnai Ninaitheerae ",
+         "12": "\nElunthar Iraivan Jeyame ",
+         "13": "\nUnga Prasanathil Siragu "
+     },
+     "Song_Link": {
+         "0": "https:\/\/www.christianslavetamil.in\/2023\/01\/yesuvae-um-mugam-parkanumae\/",
+         "1": "https:\/\/www.christianslavetamil.in\/2022\/12\/anantha-gnana-sorupa-namoo-nama\/",
+         "2": "https:\/\/www.christianslavetamil.in\/2022\/06\/kanninmani-pol-ennai-padhukathu\/",
+         "3": "https:\/\/www.christianslavetamil.in\/2022\/01\/umakaga-naan-umakaga-dhan\/",
+         "4": "https:\/\/www.christianslavetamil.in\/2021\/10\/en-adyaalm-um-mugam\/",
+         "5": "https:\/\/www.christianslavetamil.in\/2021\/09\/oh-nanba-yesu-undu\/",
+         "6": "https:\/\/www.christianslavetamil.in\/2021\/09\/idhayathin-aalam-arinthavare\/",
+         "7": "https:\/\/www.christianslavetamil.in\/2021\/09\/sali-kasakkum-thakuthi\/",
+         "8": "https:\/\/www.christianslavetamil.in\/2021\/09\/idhu-varai-seidha\/",
+         "9": "https:\/\/www.christianslavetamil.in\/2021\/09\/yesuvae-um-paasathal\/",
+         "10": "https:\/\/www.christianslavetamil.in\/2021\/09\/kaala-kaalangal-kathirundhum\/",
+         "11": "https:\/\/www.christianslavetamil.in\/2021\/09\/ennai-ninaitheerae-pathiravano\/",
+         "12": "https:\/\/www.christianslavetamil.in\/2021\/07\/elunthar-iraivan-jeyame\/",
+         "13": "https:\/\/www.christianslavetamil.in\/2020\/07\/unga-prasanathil-siragu\/"
+     }
+     , "audio_link": {
 
-                "0": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "1": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "2": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "3": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "4": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "5": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "6": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "7": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "8": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "9": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "10": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "11": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "12": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
-                "13": "Enthan Jebavelai (ft Joseph Aldrin & Keba Jeremiah)  ONE desire Vol - 2  Stephen Jebakumar.mp3",
+         "0": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "1": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "2": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "3": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "4": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "5": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "6": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "7": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "8": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "9": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "10": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "11": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "12": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
+         "13": "https://cdn.jsdelivr.net/gh/Jackcodefiles/test@main/En%20Iravo_En_Pagalo_BGM.mp3",
 
 
-            }
-        }
+     }
+ }
 
